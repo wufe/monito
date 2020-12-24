4abeef99eee19857bc10ed2d629f6793df724cfa
Simone Bembi <simone.bembi@gmail.com>
2020-12-25 00:40:00 +0100

    Simplified service worker's orchestration logic

--- Services/Monito.Worker/main.go
+++ Services/Monito.Worker/main.go
@@ -6,22 +6,17 @@ import (
 	"os"
 	"runtime"
 	"sync"
-	"time"
 
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/joho/godotenv"
 	"github.com/wufe/monito/worker/cli"
 	"github.com/wufe/monito/worker/database"
-	"github.com/wufe/monito/worker/models"
 	"github.com/wufe/monito/worker/services"
 )
 
 func main() {
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
-
-	processInterrupt := make(chan struct{})
 
 	parsedCliArguments := cli.ParseCliArguments(os.Args)
 
@@ -41,95 +36,7 @@ func main() {
 	queues := services.RegisterWorker(db, parsedCliArguments)
 	fmt.Println("Worker registered itself.")
 
-	// Resetting requests status
-	db.Model(&models.Request{}).
-		Where(&models.Request{Status: models.RequestStatusAcknowledged}).
-		Update(map[string]interface{}{"status": models.RequestStatusReady, "updated_at": time.Now()})
-
-	// Jobs channel
-	jobsByRequestType := make(map[models.RequestType]chan *models.Request)
-	jobsByRequestType[models.RequestTypeBatch] = make(chan *models.Request)
-	jobsByRequestType[models.RequestTypeSimple] = make(chan *models.Request)
-	// jobs := make(chan *models.Request)
-
-	// Build and start workers
-	startWorkers(db, queues, jobsByRequestType)
-
-	// Retrieval process
-	go func(jobsByRequestType map[models.RequestType]chan *models.Request) {
-		defer close(jobsByRequestType[models.RequestTypeBatch])
-		defer close(jobsByRequestType[models.RequestTypeSimple])
-		for {
-			select {
-			case <-processInterrupt:
-				return
-			default:
-				fmt.Println(fmt.Sprintf("[Retrieval] Looking for a request.."))
-				request := findRequest(db)
-				if request.ID == 0 {
-					fmt.Println(fmt.Sprintf("[Retrieval] No request found"))
-					time.Sleep(time.Millisecond * 2000)
-				} else {
-					fmt.Println(fmt.Sprintf("[Retrieval] Request found!"))
-					jobsByRequestType[request.Type] <- request
-					fmt.Println(fmt.Sprintf("[Retrieval] Request enqueued as job (%s)", models.GetRequestTypeLabel(request.Type)))
-				}
-			}
-		}
-	}(jobsByRequestType)
-
-	// orchestrator := services.NewQueueOrchestrator(queues, db).Init()
-
-	// fmt.Println("Orchestrator initialized.")
-
-	// orchestrator.StartQueues()
-
-	// fmt.Println("Worker started.")
-
-	// // sigs := make(chan os.Signal)
-	// // signal.Notify(sigs, syscall.SIGINT)
-	// // go func() {
-
-	// // 	<-sigs
-	// // 	fmt.Println("OK")
-	// // 	processInterrupt <- struct{}{}
-	// // 	waitGroup.Done()
-	// // }()
+	services.NewQueueOrchestrator(queues, db).Init()
 
 	waitGroup.Wait()
-
-}
-
-func findRequest(db *gorm.DB) *models.Request {
-	foundRequest := models.Request{}
-
-	db.Model(&models.Request{}).
-		Where(&models.Request{Status: models.RequestStatusReady}).
-		First(&foundRequest)
-
-	if foundRequest.ID > 0 {
-
-		db.Model(&foundRequest).
-			Where(&models.Request{ID: foundRequest.ID}).
-			Update(map[string]interface{}{"status": models.RequestStatusAcknowledged, "updated_at": time.Now()})
-	}
-
-	return &foundRequest
-}
-
-func startWorkers(db *gorm.DB, queues []*models.Queue, jobsByRequestType map[models.RequestType]chan *models.Request) {
-	for _, queue := range queues {
-
-		workingQueue := services.NewWorkingQueue(nil, queue, db)
-		requestType := workingQueue.GetRequestTypeByQueue()
-
-		go func(workingQueue *services.WorkingQueue, jobs <-chan *models.Request) {
-			for request := range jobs {
-				workingQueue.Work(request)
-			}
-		}(workingQueue, jobsByRequestType[requestType])
-
-		fmt.Println(fmt.Sprintf("Started queue with request type %s", models.GetRequestTypeLabel(requestType)))
-
-	}
 }
--- Services/Monito.Worker/services/orchestration.go
+++ Services/Monito.Worker/services/orchestration.go
@@ -1,18 +1,17 @@
 package services
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/jinzhu/gorm"
 	"github.com/wufe/monito/worker/models"
-	"github.com/wufe/monito/worker/utils"
 )
 
 type QueueOrchestrator struct {
-	queues               []*models.Queue
-	db                   *gorm.DB
-	dequeueChan          map[models.RequestType]chan *requestRetrievalQueueItem
-	availableRequestChan map[models.RequestType]chan *models.Request
+	queues            []*models.Queue
+	db                *gorm.DB
+	jobsByRequestType *map[models.RequestType]chan *models.Request
 }
 
 func NewQueueOrchestrator(queues []*models.Queue, db *gorm.DB) *QueueOrchestrator {
@@ -25,21 +24,11 @@ func NewQueueOrchestrator(queues []*models.Queue, db *gorm.DB) *QueueOrchestrato
 
 func (orchestrator *QueueOrchestrator) Init() *QueueOrchestrator {
 	orchestrator.resetRequests()
+	orchestrator.buildJobChans()
+	orchestrator.startWorkers()
 	orchestrator.startRetrievalProcess()
-	orchestrator.startDequeueProcess()
+
 	return orchestrator
-}
-
-// func (orchestrator *QueueOrchestrator) StartQueues() {
-// 	go func() {
-// 		for _, queue := range orchestrator.queues {
-// 			NewWorkingQueue(orchestrator, queue, orchestrator.db).Start()
-// 		}
-// 	}()
-// }
-
-func (orchestrator *QueueOrchestrator) GetRequest(requestType models.RequestType) chan *models.Request {
-	return orchestrator.enqueueRequestRetrieval(requestType)
 }
 
 // I supposedly have one orchestrator at a time,
@@ -51,70 +40,48 @@ func (orchestrator *QueueOrchestrator) resetRequests() {
 		Update(map[string]interface{}{"status": models.RequestStatusReady, "updated_at": time.Now()})
 }
 
-func (orchestrator *QueueOrchestrator) startDequeueProcess() {
-	dequeueChan := make(map[models.RequestType]chan *requestRetrievalQueueItem)
-	dequeueChan[models.RequestTypeSimple] = make(chan *requestRetrievalQueueItem, 1)
-	dequeueChan[models.RequestTypeBatch] = make(chan *requestRetrievalQueueItem, 1)
-	orchestrator.dequeueChan = dequeueChan
+func (orchestrator *QueueOrchestrator) buildJobChans() {
+	jobsByRequestType := make(map[models.RequestType]chan *models.Request)
+	jobsByRequestType[models.RequestTypeBatch] = make(chan *models.Request)
+	jobsByRequestType[models.RequestTypeSimple] = make(chan *models.Request)
 
-	for k := range orchestrator.dequeueChan {
-		requestType := k
-		go func() {
-			for {
-				queueItem := <-orchestrator.dequeueChan[requestType]
+	orchestrator.jobsByRequestType = &jobsByRequestType
+}
 
-				// fmt.Println(fmt.Sprintf("Waiting for a request of type %s to be found", models.GetRequestTypeLabel(requestType)))
-				foundRequest := <-orchestrator.availableRequestChan[requestType]
-				// fmt.Println(fmt.Sprintf("Request of type %s found.", models.GetRequestTypeLabel(requestType)))
+func (orchestrator *QueueOrchestrator) startWorkers() {
+	for _, queue := range orchestrator.queues {
 
-				// Return to the caller the request found
-				queueItem.returnChan <- foundRequest
+		workingQueue := NewWorkingQueue(nil, queue, orchestrator.db)
+		requestType := workingQueue.GetRequestTypeByQueue()
+
+		go func(workingQueue *WorkingQueue, jobs <-chan *models.Request) {
+			for request := range jobs {
+				workingQueue.Work(request)
 			}
-		}()
+		}(workingQueue, (*orchestrator.jobsByRequestType)[requestType])
+
+		fmt.Println(fmt.Sprintf("Started queue with request type %s", models.GetRequestTypeLabel(requestType)))
+
 	}
 }
 
 func (orchestrator *QueueOrchestrator) startRetrievalProcess() {
-	availableRequestChanDictionary := make(map[models.RequestType]chan *models.Request)
-	availableRequestChanDictionary[models.RequestTypeBatch] = make(chan *models.Request, 1)
-	availableRequestChanDictionary[models.RequestTypeSimple] = make(chan *models.Request, 1)
-	orchestrator.availableRequestChan = availableRequestChanDictionary
-
-	utils.SetInterval(orchestrator.retrievalProcess, 2000, false)
-}
-
-func (orchestrator *QueueOrchestrator) retrievalProcess() {
-
-	request := orchestrator.findRequest()
-	if request.ID == 0 {
-		// fmt.Println("Not found")
-	} else {
-
-		orchestrator.availableRequestChan[request.Type] <- request
-		// fmt.Println(fmt.Sprintf("Found (%s)!", models.GetRequestTypeLabel(request.Type)))
-	}
-
-	// fmt.Println("Orchestrator 5s timeout")
-}
-
-type requestRetrievalQueueItem struct {
-	requestType models.RequestType
-	returnChan  chan *models.Request
-}
-
-func (orchestrator *QueueOrchestrator) enqueueRequestRetrieval(requestType models.RequestType) chan *models.Request {
-	returnChan := make(chan *models.Request)
-	// Building a Go func in order to do not wait
-	// for the request to be enqueued.
-	// Might use buffered channel but since the traffic is not known,
-	// the buffer size is also not known.
-	go func() {
-		orchestrator.dequeueChan[requestType] <- &requestRetrievalQueueItem{
-			requestType: requestType,
-			returnChan:  returnChan,
+	go func(jobsByRequestType map[models.RequestType]chan *models.Request) {
+		defer close(jobsByRequestType[models.RequestTypeBatch])
+		defer close(jobsByRequestType[models.RequestTypeSimple])
+		for {
+			fmt.Println(fmt.Sprintf("[Retrieval] Looking for a request.."))
+			request := orchestrator.findRequest()
+			if request.ID == 0 {
+				fmt.Println(fmt.Sprintf("[Retrieval] No request found"))
+				time.Sleep(time.Millisecond * 2000)
+			} else {
+				fmt.Println(fmt.Sprintf("[Retrieval] Request found!"))
+				jobsByRequestType[request.Type] <- request
+				fmt.Println(fmt.Sprintf("[Retrieval] Request enqueued as job (%s)", models.GetRequestTypeLabel(request.Type)))
+			}
 		}
-	}()
-	return returnChan
+	}(*orchestrator.jobsByRequestType)
 }
 
 func (orchestrator *QueueOrchestrator) findRequest() *models.Request {
